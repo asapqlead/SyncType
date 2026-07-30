efbcf027cfdd826763256fe89f8c0596c22f849a
asapqlead <chhomalead@gmail.com>
2026-07-30 12:35:39 +0700

    feat: Config deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.VITE_API_BASE': JSON.stringify(process.env.VITE_API_BASE || 'http://127.0.0.1:8000/api')
-  }
 })
